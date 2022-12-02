7fd4af5bb7f2e682468bdf490794c9793f359c04
moralejo20 <mjose14957@gmail.com>
2022-12-01 21:36:57 -0600

    parallax update
    parallax update

--- SpaceExplorer/Assets/Scripts/ParallaxEfect.cs
+++ SpaceExplorer/Assets/Scripts/ParallaxEfect.cs
@@ -4,9 +4,9 @@ using UnityEngine;
 
 public class ParallaxEfect : MonoBehaviour
 {
-
-    private Transform cameraTransform;
-    private Vector3 previousCameraPosition;
+    
+    private Transform cameraTransform;//referencia al componete de transform de la camera 
+    private Vector3 previousCameraPosition;//posicion anterior de la camara
     // Start is called before the first frame update
     void Start()
     {
@@ -19,7 +19,8 @@ public class ParallaxEfect : MonoBehaviour
     // Update is called once per frame
     void LateUpdate()
     {
-        float deltaX = (cameraTransform.position.x - previousCameraPosition.x)*0.3f;
+        float deltaX = (cameraTransform.position.x - previousCameraPosition.x)*0.5f;
         transform.Translate(new Vector3(deltaX, 0, 0));
+        previousCameraPosition = cameraTransform.position;
     }
 }
