a3b3827eb7c985a8554363ea3f61d6f4c2e989f2
moralejo20 <mjose14957@gmail.com>
2022-12-02 23:33:56 -0600

    update

--- SpaceExplorer/Assets/Scripts/ParallaxEfect.cs
+++ SpaceExplorer/Assets/Scripts/ParallaxEfect.cs
@@ -3,15 +3,20 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class ParallaxEfect : MonoBehaviour
+
 {
+    [SerializeField] private float parallaxMutiplier;
     
     private Transform cameraTransform;//referencia al componete de transform de la camera 
     private Vector3 previousCameraPosition;//posicion anterior de la camara
+    private float spriteWidth, startPosition;
     // Start is called before the first frame update
     void Start()
     {
         cameraTransform = Camera.main.transform;
         previousCameraPosition = cameraTransform.position;
+        spriteWidth = GetComponent<SpriteRenderer>().bounds.size.x;
+        startPosition = transform.position.x;
 
 
     }
@@ -19,8 +24,20 @@ public class ParallaxEfect : MonoBehaviour
     // Update is called once per frame
     void LateUpdate()
     {
-        float deltaX = (cameraTransform.position.x - previousCameraPosition.x)*0.5f;
+        float deltaX = (cameraTransform.position.x - previousCameraPosition.x)* parallaxMutiplier;
+        float moveAmount = cameraTransform.position.x * (1 - parallaxMutiplier);
+
         transform.Translate(new Vector3(deltaX, 0, 0));
         previousCameraPosition = cameraTransform.position;
+
+        if (moveAmount > startPosition + spriteWidth)
+        {
+            transform.Translate(new Vector3(spriteWidth, 0, 0));
+            startPosition += spriteWidth;
+        }
+        else if (moveAmount < startPosition - spriteWidth) {
+            transform.Translate(new Vector3(-spriteWidth, 0, 0));
+            startPosition -= spriteWidth;
+        }
     }
 }
