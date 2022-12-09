9438c09cf2be348eba678e2fe92fe4bae58c2174
Darv9 <daniel.rave0903@gmail.com>
2022-12-08 20:38:09 -0600

    revierte un commit

--- SpaceExplorer/Assets/Scripts/ParallaxEfect.cs
+++ SpaceExplorer/Assets/Scripts/ParallaxEfect.cs
@@ -5,11 +5,17 @@ using UnityEngine;
 public class ParallaxEfect : MonoBehaviour
 
 {
+<<<<<<< HEAD
     [SerializeField] private float parallaxMutiplier;
     
     private Transform cameraTransform;//referencia al componete de transform de la camera 
     private Vector3 previousCameraPosition;//posicion anterior de la camara
     private float spriteWidth, startPosition;
+=======
+
+    private Transform cameraTransform;
+    private Vector3 previousCameraPosition;
+>>>>>>> parent of 7fd4af5 (parallax update)
     // Start is called before the first frame update
     void Start()
     {
@@ -24,6 +30,7 @@ public class ParallaxEfect : MonoBehaviour
     // Update is called once per frame
     void LateUpdate()
     {
+<<<<<<< HEAD
         float deltaX = (cameraTransform.position.x - previousCameraPosition.x)* parallaxMutiplier;
         float moveAmount = cameraTransform.position.x * (1 - parallaxMutiplier);
 
@@ -39,5 +46,9 @@ public class ParallaxEfect : MonoBehaviour
             transform.Translate(new Vector3(-spriteWidth, 0, 0));
             startPosition -= spriteWidth;
         }
+=======
+        float deltaX = (cameraTransform.position.x - previousCameraPosition.x)*0.3f;
+        transform.Translate(new Vector3(deltaX, 0, 0));
+>>>>>>> parent of 7fd4af5 (parallax update)
     }
 }
