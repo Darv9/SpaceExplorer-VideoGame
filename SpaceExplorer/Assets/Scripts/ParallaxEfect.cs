d6ac491752d43d8a03e25de2d18a894a8c1b638b
Darv9 <daniel.rave0903@gmail.com>
2022-12-08 21:03:32 -0600

    Revert "revierte un commit"
    This reverts commit 9438c09cf2be348eba678e2fe92fe4bae58c2174.

--- SpaceExplorer/Assets/Scripts/ParallaxEfect.cs
+++ SpaceExplorer/Assets/Scripts/ParallaxEfect.cs
@@ -5,17 +5,11 @@ using UnityEngine;
 public class ParallaxEfect : MonoBehaviour
 
 {
-<<<<<<< HEAD
     [SerializeField] private float parallaxMutiplier;
     
     private Transform cameraTransform;//referencia al componete de transform de la camera 
     private Vector3 previousCameraPosition;//posicion anterior de la camara
     private float spriteWidth, startPosition;
-=======
-
-    private Transform cameraTransform;
-    private Vector3 previousCameraPosition;
->>>>>>> parent of 7fd4af5 (parallax update)
     // Start is called before the first frame update
     void Start()
     {
@@ -30,7 +24,6 @@ public class ParallaxEfect : MonoBehaviour
     // Update is called once per frame
     void LateUpdate()
     {
-<<<<<<< HEAD
         float deltaX = (cameraTransform.position.x - previousCameraPosition.x)* parallaxMutiplier;
         float moveAmount = cameraTransform.position.x * (1 - parallaxMutiplier);
 
@@ -46,9 +39,5 @@ public class ParallaxEfect : MonoBehaviour
             transform.Translate(new Vector3(-spriteWidth, 0, 0));
             startPosition -= spriteWidth;
         }
-=======
-        float deltaX = (cameraTransform.position.x - previousCameraPosition.x)*0.3f;
-        transform.Translate(new Vector3(deltaX, 0, 0));
->>>>>>> parent of 7fd4af5 (parallax update)
     }
 }
