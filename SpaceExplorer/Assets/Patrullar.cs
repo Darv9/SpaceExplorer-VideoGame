6f3333af37ee3046c762b017e74c4508fb698fc5
moralejo20 <mjose14957@gmail.com>
2022-12-11 13:29:17 -0600

    EnemyUpdate
    EnemyUpdate

---
+++ SpaceExplorer/Assets/Patrullar.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Patrullar : MonoBehaviour
+{
+    [SerializeField] private float velocidadMovimiento;
+    [SerializeField] private Transform[] puntosMovimiento;
+
+    [SerializeField] private float distanciaMinima;
+
+    private int numeroAleatorio;
+
+}
